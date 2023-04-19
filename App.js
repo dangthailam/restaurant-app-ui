2e66c6612a6cda924b0375376f150246fda466f4
Lam DANG <lamtp1989@gmail.com>
2023-04-19 23:38:48 +0200

    init menu bars

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Button, Alert } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={[
+        styles.container,
+        {
+          flexDirection: "column",
+        },
+      ]}
+    >
+      <View style={{ flex: 9, flexDirection: "row" }}>
+        <View style={{ flex: 9 }} />
+        <View style={{ flex: 1, backgroundColor: "gray" }}>
+          {verticalSidebarButtons.map((el) => (
+            <>
+              <Ionicons name="md-checkmark-circle" size={32} color="green" />
+              <Button
+                onPress={() => Alert.alert("Simple Button pressed")}
+                title={el.title}
+                color="#841584"
+                accessibilityLabel="Learn more about this purple button"
+              />
+            </>
+          ))}
+        </View>
+      </View>
+      <View style={{ flex: 1, backgroundColor: "gray" }} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+const verticalSidebarButtons = [
+  {
+    title: "Réservations",
+  },
+  {
+    title: "Clients",
+  },
+  {
+    title: "Réglages",
+  },
+  {
+    title: "Service",
+  },
+];
+
+export default App;
